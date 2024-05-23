1bf003048453f6837b396b7672d8cea1b4171182
Matthew O'Rourke <matthew.orourke@travelperk.com>
2024-05-23 11:27:31 +0100

    Initial commit

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// Add polyfill for AbortSignal.timeout.
+// https://github.com/jsdom/jsdom/issues/3516
+if (!AbortSignal.timeout) {
+  AbortSignal.timeout = (ms) => {
+    const controller = new AbortController();
+    setTimeout(() => controller.abort(new DOMException("TimeoutError")), ms);
+    return controller.signal;
+  };
+}
